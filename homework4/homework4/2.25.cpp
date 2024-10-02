4690202cab1a78c43a3b06ac74f499c1a67b2216
52-cheng <rxu360271@gmail.com>
2024-10-02 22:23:46 +0800

    ok

---
+++ homework4/homework4/2.25.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf(" hhhhhhhhh\n");
+    printf("     h    \n");
+    printf("     h    \n");
+    printf("     h    \n");
+    printf("     h    \n");
+    printf("     h    \n");
+    printf("     h    \n\n");
+
+    
+
+    printf("  jj       \n");
+    printf(" j        \n");
+    printf("  j       j\n");
+    printf("   jjjjjjjj\n");
+    printf("          j\n\n");
+
+    
+    printf("  ccccccccc\n");
+    printf(" c         c\n");
+    printf(" c         c\n");
+    printf("  c       c\n");
+    return 0;
+}
